55cdcc5851481ab176a634dcf02d7af888eb8f70
ORomig <oliviaromig7@gmail.com>
2022-11-02 09:32:33 -0500

    Added pictures

---
+++ exercise02/script.js
@@ -0,0 +1,10 @@
+//goes to previous picture
+const images =["cyclamen.webp", "tree.jpg", "orchid.jpeg", "pinkBlueFlowers.webp", "blueFlower.jpeg", "cherry.jpeg"]
+function prev(){
+
+}
+
+//goes to next picture
+function next(){
+
+}
